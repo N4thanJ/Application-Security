3b6b1dfa18b8128f276b15c8eba82e716825a017
Meast04 <leenen.maxime@gmail.com>
2024-12-16 10:32:05 +0100

    iets snap niey wat

--- front-end/components/shoppingcart/AddShoppingcartForm.tsx
+++ front-end/components/shoppingcart/AddShoppingcartForm.tsx
@@ -1,7 +1,7 @@
 import ShoppingcartService from '@services/ShopingcartService';
 import { Shoppingcart } from '@types';
 import { useRouter } from 'next/router';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 
 const AddShoppingcartForm: React.FC = () => {
     const router = useRouter();
@@ -30,24 +30,6 @@ const AddShoppingcartForm: React.FC = () => {
             console.error('Error adding shoppingcart:', error);
         }
     };
-
-    const handleDateClick = () => {
-        if (dateInputRef.current) {
-            dateInputRef.current.showPicker();
-        }
-    };
-
-    useEffect(() => {
-        if (dateInputRef.current) {
-            dateInputRef.current.addEventListener('click', handleDateClick);
-        }
-
-        return () => {
-            if (dateInputRef.current) {
-                dateInputRef.current.removeEventListener('click', handleDateClick);
-            }
-        };
-    }, []);
 
     return (
         <form onSubmit={handleSubmit} className="max-w-md mx-auto">
@@ -82,10 +64,9 @@ const AddShoppingcartForm: React.FC = () => {
                             required
                             name="deliveryDate"
                             value={
-                                new Date(new Date().setDate(new Date().getDate() + 1))
-                                    .toISOString()
-                                    .split('T')[0] ||
-                                shoppingcart.deliveryDate.toISOString().split('T')[0]
+                                shoppingcart.deliveryDate
+                                    ? new Date(shoppingcart.deliveryDate).toISOString().split('T')[0]
+                                    : ''
                             }
                             onChange={handleInputChange}
                             className="block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 cursor-pointer"
