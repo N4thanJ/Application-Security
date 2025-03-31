fef3109313e1b151b636145859f23e26f76d9767
N4thanJ <nathan.jordens@student.ucll.be>
2025-03-31 15:42:58 +0200

    Changed some settings for better pentesting

--- back-end/app.ts
+++ back-end/app.ts
@@ -16,7 +16,7 @@ app.use(helmet());
 dotenv.config();
 const port = process.env.APP_PORT || 3000;
 
-app.use(cors({ origin: 'http://localhost:8080' }));
+app.use(cors({ origin: ['http://localhost:8000', 'http://127.0.0.1:8000'] }));
 app.use(express.json());
 
 // Swagger docs
